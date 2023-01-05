a9a1683b2f8941125e9438681c2fee344b5f718d
Monoral <goldflesher@gmail.com>
2023-01-05 16:50:00 +0600

    Добавление рандомайзера для id пользователя и для поста.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,6 +67,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :keywords, images: [])
+      params.require(:post).permit(:title, :description, :keywords, :user_id, images: [])
     end
 end
